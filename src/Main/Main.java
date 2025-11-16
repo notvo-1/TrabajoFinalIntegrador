eb4602522dc1fa75e92db5f6adc617628c4e5bf1
Nicolas Pagola <nicolaspagola@outlook.com>
2025-11-16 13:28:20 -0300

    Main inicializa AppMenu

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -14,7 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        AppMenu app = new AppMenu();
+        app.run();
     }
     
 }
